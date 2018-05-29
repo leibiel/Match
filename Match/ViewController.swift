84414e6b9bcadd6891ee5bf413538e57fb47691a
Gabriel Campos <39700818+leibiel@users.noreply.github.com>
2018-05-28 22:49:37 -0300

    Alterações para criação do layout de tabela de acordo com a aula 5.

--- Match/ViewController.swift
+++ Match/ViewController.swift
@@ -8,16 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
     
     var model = CardModel()
     var cardArray = [Card]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    
         
         // Call the getCards method of the card model
         cardArray = model.getCards()
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    
     
     }
 
@@ -26,6 +33,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    // MARK: - UICollectionView Protocol Methods
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return  cardArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CardCell", for: indexPath)
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+    }
 }
 
